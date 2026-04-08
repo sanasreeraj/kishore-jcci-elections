eaec3a821da83dc5332c562a182ccb7cda234453
Sana Sreeraj <iamsanasreeraj@gmail.com>
2026-04-08 18:38:51 +0530

    Restore highlighted emphasis in voting rules

--- src/components/campaign-site.tsx
+++ src/components/campaign-site.tsx
@@ -788,7 +788,7 @@ export function CampaignSite({
               <ul className={styles.ruleList}>
                 {election.rules.map((rule, index) => (
                   <li key={rule.text}>
-                    {renderHighlightedText(activeCopy.electionRules[index] ?? rule.text, [])}
+                    {renderHighlightedText(activeCopy.electionRules[index] ?? rule.text, rule.highlights)}
                   </li>
                 ))}
               </ul>
